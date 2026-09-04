0dab32f7352bc4689010cf874f372aff2ea8c549
Deepanki-sketch <25f3002767@ds.study.iitm.ac.in>
2026-09-04 22:56:54 +0530

    quicksort_try

---
+++ concepts/quicksort.py
@@ -0,0 +1,17 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]  # Choose the middle element as the pivot
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quicksort(left) + middle + quicksort(right)
+
+
+# Example usage
+numbers = [10, 7, 8, 9, 1, 5]
+sorted_numbers = quicksort(numbers)
+
+print(sorted_numbers)
